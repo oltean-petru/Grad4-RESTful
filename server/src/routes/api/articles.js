936592556b54f8957320fb87da3b98fac649d635
Evan <marcel.evan2001@gmail.com>
2024-02-13 14:26:52 +0100

    fix route

--- server/src/routes/api/articles.js
+++ server/src/routes/api/articles.js
@@ -4,15 +4,8 @@ import articleController from '#src/controllers/articleController';
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-  const allArticles = articleController.allArticleController()
-  res.json(allArticles);
-});
+router.get('/', articleController.allArticleController);
 
-router.post('/', (req, res) => {
-  const article = req.body
-  const newArticle = articleController.createArticle(article)
-  res.json(newArticle);
-});
+router.post('/', articleController.createArticle);
 
 export default router;
