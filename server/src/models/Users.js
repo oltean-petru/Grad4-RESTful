58a3ede5519decf0f93fcfe645abcccd7ed236bc
Evan <marcel.evan2001@gmail.com>
2024-02-13 10:24:40 +0100

    change model user

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -6,10 +6,10 @@ const userSchema = new Schema({
     lastName : String , 
     firstName: String,
     email: { type:String,  required:'un nom est obligatoire:)' },
-    password: { type:String, required:'un mot de passe est obligatoire:)' },
+    password: { type:String, minLength:3, maxLength:20 },
     salt: String,
     creationDate: { type: Date, default: Date.now },
-    roles: { type: Array}
+    roles: [Schema.Types.ObjectId]
 });
 
 const userModel = mongoose.model('users',userSchema)
