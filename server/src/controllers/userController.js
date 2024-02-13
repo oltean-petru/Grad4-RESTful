c04552a418ef902ca7ccc19d7ea40d4bca6b71d7
Oltean Petru <poltean33@gmail.com>
2024-02-13 11:56:31 +0100

    bcrypt

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -6,13 +6,13 @@ const exposeController = {
         return res.json(allUsers)
     },
 
-    createUser:async (req,res)=>{
-        const {body}  = req
+    createUser: async (req, res) => {
+        const { body } = req
         try {
-                const registeredUser = await usersService.createUser(body)     
-                return res.json(registeredUser)
-            } catch (error) {
-               return res.sendStatus(400)
+            const registeredUser = await usersService.createUser(body)
+            return res.json(registeredUser)
+        } catch (error) {
+            return res.sendStatus(400)
         }
     }
 }
