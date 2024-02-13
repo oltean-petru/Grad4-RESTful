238286b4e77f571adb5a0fbc8e5c40cb6f44d6b0
Evan <marcel.evan2001@gmail.com>
2024-02-13 10:21:59 +0100

    bracket

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,7 +2,7 @@ import usersServices from '#src/services/userServices';
 
 const exposeController = {
     allUserController: async (req, res) => {
-        const allUsers = await usersServices.findAllUsers;
+        const allUsers = await usersServices.findAllUsers();
         return res.json(allUsers)
     }
 }
