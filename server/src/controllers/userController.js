c03da8c32088bea09e35709906ae14edd65cb9a7
Evan <marcel.evan2001@gmail.com>
2024-02-13 12:32:09 +0100

    fix

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -14,16 +14,6 @@ const exposeController = {
         } catch (error) {
             return res.sendStatus(400)
         }
-    },
-
-    AuthLogin: async (req, res) => {
-        const { body } = req
-        try {
-            const registeredUser = await usersService.login(body)
-            return res.json(registeredUser)
-        } catch (error) {
-            return res.sendStatus(400)
-        }
     }
 }
 
