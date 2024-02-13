c201cb49de642fdd698c389b45d193229b61b543
Evan <marcel.evan2001@gmail.com>
2024-02-13 14:09:19 +0100

    fix auth

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,13 +1,14 @@
 import authService from '#src/services/authService';
 
 const exposeController = {
-    authLogin: async (req, res) => {
-        const { body } = req
+    
+    authLogin :async (req,res)=>{
+        const {body} = req
         const user = await authService.authLogin(body)
-        if (!user) {
-            return res.sendStatus(401)
-        }
-        return res.json(registeredUser)
+        if(!user) return res.sendStatus(401)
+        const testMatch = await authService.comparePassword({password:body.password,storedPassword:user.password})
+        if(testMatch) return res.json({login:'success!'})
+        return res.sendStatus(401)
     }
 }
 
--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -4,17 +4,18 @@ import bcrypt from 'bcrypt';
 
 const exposeServices = {
 
-authLogin: async (rawData) => { 
-    const { email, password } = rawData
-    const user = await User.findOne({ email })
-    if (!user) {
-      throw new Error('User not found')
+  authLogin: async ({ email, password }) => {
+    try {
+      const findUser = await User.findOne({ email })
+      return findUser
+    } catch (error) {
+      throw new Error(error)
     }
-    const hash = await bcrypt.hash(password, user.salt)
-    if (hash !== user.password) {
-      throw new Error('Bad password')
-    }
-    return user
+  },
+
+  comparePassword: async ({ password, storedPassword }) => {
+    const result = await bcrypt.compare(password, storedPassword);
+    return result
   }
 }
 
