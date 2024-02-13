f78c732698649da48a9df8cce10ae6c83d52cf1e
Evan <marcel.evan2001@gmail.com>
2024-02-13 10:07:34 +0100

    add articles route & user controller

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,8 @@
+const exposeController = {
+    allUserController:(req, res) => {
+        return res.json({yeah : 'yeah'});
+    }
+}
+
+
+export default exposeController;
---
+++ server/src/routes/api/articles.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/articles', (req, res) => {
+
+});
+
+export default router;
