07f116a0829587f03663ef5025aa7e6c886d3aaa
Evan <marcel.evan2001@gmail.com>
2024-02-13 12:33:01 +0100

    fix

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -4,7 +4,7 @@ const exposeController = {
     authLogin: async (req, res) => {
         const { body } = req
         try {
-            const registeredUser = await usersService.login(body)
+            const registeredUser = await authService.authLogin(body)
             return res.json(registeredUser)
         } catch (error) {
             return res.sendStatus(400)
